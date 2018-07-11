19a29c7fd52ff233abe74dcc3ffee460a0e52627
xing <1741807421@qq.com>
2018-07-11 18:22:23 +0800

    修改密码

--- account/urls.py
+++ account/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from django.contrib.auth.views import login, logout, logout_then_login
+from django.contrib.auth.views import login, logout, logout_then_login, password_change, password_change_done
 from . import views
 
 
@@ -9,4 +9,6 @@ urlpatterns = [
     url(r'^login/$', login, name='login'),
     url(r'^logout/$', logout, name='logout'),
     url(r'^logout-then-login/$', logout_then_login, name='logout_then_login'),
+    url(r'^password_change/$', password_change, name='password_change'),
+    url(r'^password_change/done/$', password_change_done, name='password_change_done'),
 ]
