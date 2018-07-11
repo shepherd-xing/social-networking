71ec5d4812f3fa7ed7b10fee1f53066792caf78c
xing <1741807421@qq.com>
2018-07-11 19:05:54 +0800

    重置密码

--- account/urls.py
+++ account/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
-from django.contrib.auth.views import login, logout, logout_then_login, password_change, password_change_done
+from django.contrib.auth.views import login, logout, logout_then_login, password_change, \
+    password_change_done, password_reset, password_reset_done, password_reset_confirm, password_reset_complete
 from . import views
 
 
@@ -11,4 +12,9 @@ urlpatterns = [
     url(r'^logout-then-login/$', logout_then_login, name='logout_then_login'),
     url(r'^password_change/$', password_change, name='password_change'),
     url(r'^password_change/done/$', password_change_done, name='password_change_done'),
+    url(r'^password-reset/$', password_reset, name='password_reset'),
+    url(r'^password-reset/done/$', password_reset_done, name='password_reset_done'),
+    url(r'^password-reset/confirm/(?P<uidb64>[-\w]+)/(?P<token>[-\w]+)/$',
+        password_reset_confirm, name='password_reset_confirm'),
+    url(r'^password-reset/complete/$', password_reset_complete, name='password_reset_complete'),
 ]
