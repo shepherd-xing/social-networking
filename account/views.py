eef4f7448b7c0279f9242c38d01b5827f28ef774
xing <1741807421@qq.com>
2018-07-11 18:06:42 +0800

    注册登录

---
+++ account/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from .forms import LoginForm
+
+# Create your views here.
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(username=cd['username'], password=cd['password'])
+
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse('验证成功')
+                else:
+                    return HttpResponse('休眠账户')
+            else:
+                return HttpResponse('非法登录')
+    else:
+        form = LoginForm()
+
+    return render(request, 'account/login.html', {'form':form})
+
+@login_required
+def dashboard(request):
+    return render(request, 'account/dashboard.html', {'section':'dashboard'})
+
+
+
+
+
+
+
+
+
+
+
+
