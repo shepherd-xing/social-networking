6619787764e05c9b31faa44556019410915f2b91
xing <1741807421@qq.com>
2018-07-11 19:34:52 +0800

    注册

--- account/forms.py
+++ account/forms.py
@@ -1,5 +1,20 @@
 from django import forms
+from django.contrib.auth.models import User
 
 class LoginForm(forms.Form):
     username = forms.CharField(label='用户名')
     password = forms.CharField(label='密码', widget=forms.PasswordInput)
+
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(label='密码', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='重复密码', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'email')
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError('密码不一致。')
+        return cd['password2']
