bf3cb453a59ec003d60a50d00ba11cd6e0669cd1
xing <1741807421@qq.com>
2018-07-12 16:15:31 +0800

    从其它网站上传内容

---
+++ images/forms.py
@@ -0,0 +1,44 @@
+from django import forms
+from urllib import request
+from django.core.files.base import ContentFile
+from django.utils.text import slugify
+from .models import Image
+
+class ImageCreateForm(forms.ModelForm):
+    class Meta:
+        model = Image
+        fields = ('title', 'url', 'description')
+        widgets = {
+            'url':forms.HiddenInput,
+        }
+
+    def clean_url(self):
+        url = self.cleaned_data['url']
+        valid_extensions = ['jpg', 'jpeg']
+        extension = url.rsplit('.', 1)[1].lower()
+        if extension not in valid_extensions:
+            raise forms.ValidationError('URL不匹配有效的图片扩展名。')
+        return url
+
+    def save(self, force_insert=False, force_update=False, commit=True):
+        image = super().save(commit=False)
+        image_url = self.cleaned_data['url']
+        image_name = '{}.{}'.format(slugify(image.title), image_url.rsplit('.', 1)[1].lower())
+
+        # 下载图片
+        response = request.urlopen(image_url)
+        image.image.save(image_name, ContentFile(response.read()), save=False)
+        if commit:
+            image.save()
+        return image
+
+
+
+
+
+
+
+
+
+
+
