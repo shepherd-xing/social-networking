4ddda1bf7783f2fbe5b754756ab738ef2943a49a
xing <1741807421@qq.com>
2018-07-13 13:42:05 +0800

    中介模型

--- account/models.py
+++ account/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -9,3 +10,28 @@ class Profile(models.Model):
     photo = models.ImageField('照片', upload_to='users/%Y/%m/%d', blank=True)
     def __str__(self):
         return '{}的个人资料'.format(self.user.username)
+
+class Contact(models.Model):
+    user_from = models.ForeignKey(User, related_name='rel_from_set')
+    user_to = models.ForeignKey(User, related_name='rel_to_set')
+    created = models.DateTimeField(auto_now_add=True, db_index=True)
+
+    class Meta:
+        ordering = ('-created',)
+    def __str__(self):
+        return '{} follows {}'.format(self.user_from, self.user_to)
+
+User.add_to_class('following', models.ManyToManyField('self', through=Contact, related_name='followers',
+                                                      symmetrical=False))
+
+
+
+
+
+
+
+
+
+
+
+
